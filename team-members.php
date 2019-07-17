c399f3ffe0c78af4bb69ab39152434e9e3ffbeef
oktaryan-h <oktaryan.h@gmail.com>
2019-07-17 15:45:11 +0700

    Team Members R5

--- team-members.php
+++ team-members.php
@@ -12,7 +12,7 @@ License: GPLv2
 
 class Team_members {
 
-	function create_movie_review() {
+	function create_team_member() {
 
 		register_post_type( 'team_members',
 			array(
@@ -42,57 +42,6 @@ class Team_members {
 		);
 	}
 
-	function wp_team_members_metabox( $meta_boxes ) {
-
-		$prefix = 'mb-';
-
-		$meta_boxes[] = array(
-			'id' => 'untitled',
-			'title' => esc_html__( 'Untitled Metabox', 'metabox-online-generator' ),
-			'post_types' => array('team_members'),
-			'context' => 'advanced',
-			'priority' => 'default',
-			'autosave' => 'false',
-			'fields' => array(
-				array(
-					'id' => $prefix . 'position',
-					'name' => esc_html__( 'Position', 'metabox-online-generator' ),
-					'type' => 'select',
-					'placeholder' => esc_html__( 'Select an Item', 'metabox-online-generator' ),
-					'options' => array(
-						'owner' => 'Owner',
-						'manager' => 'Manager',
-						'design' => 'Design Dept.',
-					),
-				),
-				array(
-					'id' => $prefix . 'email',
-					'name' => esc_html__( 'Email', 'metabox-online-generator' ),
-					'type' => 'email',
-				),
-				array(
-					'id' => $prefix . 'phone',
-					'type' => 'text',
-					'name' => esc_html__( 'Phone', 'metabox-online-generator' ),
-				),
-				array(
-					'id' => $prefix . 'website',
-					'type' => 'url',
-					'name' => esc_html__( 'Website', 'metabox-online-generator' ),
-				),
-				array(
-					'id' => $prefix . 'image',
-					'type' => 'image_select',
-					'name' => esc_html__( 'Image Select', 'metabox-online-generator' ),
-					'force_delete' => 'false',
-					'max_file_uploads' => '4',
-				),
-			),
-		);
-
-		return $meta_boxes;
-	}
-
 	function native_metabox() {
 		add_meta_box(
             'wporg_box_id',           // Unique ID
@@ -103,21 +52,22 @@ class Team_members {
 	}
 
 	function native_metabox_html($post) {
-		$a = get_post_meta($post->ID);
+		//$a = get_post_meta($post->ID);
+		$a = $post->ID;
 		//var_dump($a);
 		?>
-		<input name="mb-position" type="text" value="<?php echo $a['mb-position'][0] ?>">
-		<input name="mb-email" type="text" value="<?php echo $a['mb-email'][0] ?>">
-		<input name="mb-phone" type="text" value="<?php echo $a['mb-phone'][0] ?>">
+		<input name="mb-position" type="text" value="<?php echo get_post_meta($a,'mb-position',true) ?>">
+		<input name="mb-email" type="text" value="<?php echo get_post_meta($a,'mb-email',true) ?>">
+		<input name="mb-phone" type="text" value="<?php echo get_post_meta($a,'mb-phone',true) ?>">
 		<?php
 	}
 
 	function save_metabox($post_id) {
     //if (array_key_exists('wporg_field', $_POST)) {
-        if (isset($_POST['mb-position'])) update_post_meta($post_id,'mb-position',$_POST['mb-position']);
-        if (isset($_POST['mb-email'])) update_post_meta($post_id,'mb-email',$_POST['mb-email']);
-        if (isset($_POST['mb-phone'])) update_post_meta($post_id,'mb-phone',$_POST['mb-phone']);
-    }
+		if (isset($_POST['mb-position'])) update_post_meta($post_id,'mb-position',$_POST['mb-position']);
+		if (isset($_POST['mb-email'])) update_post_meta($post_id,'mb-email',$_POST['mb-email']);
+		if (isset($_POST['mb-phone'])) update_post_meta($post_id,'mb-phone',$_POST['mb-phone']);
+	}
 
 	function show_team_members($attr) {
 
@@ -147,9 +97,9 @@ class Team_members {
 				echo '<li>'.get_post_meta(get_the_ID(),'mb-image',true).'</li>';
 				echo '<li>'.get_the_title().'</li>';
 				echo '<li><strong>'.get_post_meta(get_the_ID(),'mb-position',true).'</strong></li>';
-				if ($a['email'] != 'false') echo '<li>'.get_post_meta(get_the_ID(),'mb-email',true).'</li>';
-				if ($a['phone'] != 'false') echo '<li>'.get_post_meta(get_the_ID(),'mb-phone',true).'</li>';
-				if ($a['website'] != 'false') echo '<li>'.get_post_meta(get_the_ID(),'mb-website',true).'</li>';
+				if (isset($a['email'])) if ($a['email'] != 'false') echo '<li>'.get_post_meta(get_the_ID(),'mb-email',true).'</li>' ;
+				if (isset($a['phone'])) if ($a['phone'] != 'false') echo '<li>'.get_post_meta(get_the_ID(),'mb-phone',true).'</li>';
+				if (isset($a['website'])) if ($a['website'] != 'false') echo '<li>'.get_post_meta(get_the_ID(),'mb-website',true).'</li>' ;
 
 			}
 
@@ -170,12 +120,12 @@ class Team_members {
 
 $team_members = new Team_members;
 
-add_action('init', array($team_members,'create_movie_review'));
+add_action('init', array($team_members,'create_team_member'));
 
 add_action('add_meta_boxes', array($team_members,'native_metabox'));
-add_action('save_post', array($team_members,'save_metabox'));
+add_action('save_post_team_members', array($team_members,'save_metabox'));
 
-add_filter('rwmb_meta_boxes', array($team_members,'wp_team_members_metabox'));
+//add_filter('rwmb_meta_boxes', array($team_members,'wp_team_members_metabox'));
 add_shortcode('team-members', array($team_members, 'show_team_members'));
 
 //add_action('init', [new Team_members, 'create_movie_review']);
